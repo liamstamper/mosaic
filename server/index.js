c6faa189e11c5867d9773dd48f8ccc7bf166c947
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-12 09:17:47 -0500

    routes set up

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 // Configs
 const __filename = fileURLToPath(import.meta.url);
@@ -35,10 +36,11 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-// Routes  
+// Routes with files
 app.post("/auth/register", upload.single("picture"), register);
 
-
+// Routes
+app.use("/auth", authRoutes);
 
 // Mongoose setup
 const PORT = process.env.PORT || 3001;
