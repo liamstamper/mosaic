d68f1d2c0f35f8a1e1c99ba0ca031e45528de79a
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-09-14 11:36:44 -0500

    added post routes

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import { verifyToken } from "./middleware/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js";
 
 // Configs
 const __filename = fileURLToPath(import.meta.url);
@@ -44,9 +46,12 @@ app.use("/users", userRoutes)
 
 // Routes with files
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // Routes
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", verifyToken, postRoutes);
 
 // Mongoose setup
 const PORT = process.env.PORT || 3001;
